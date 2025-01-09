4576458040f14dcba709ae83cf6f582504480866
dac <dac@dypm.org>
2025-01-09 11:34:09 +0530

    addition to multiplication changes

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ public class Test{
    public static void main(string args[]){
     int a=10;
     int b=11;
-    System.out.println("addition of two number"+(a+b));
+    System.out.println("multipliction of two number"+(a*b));
    }
 }
